ff2a8ea6a2b47057217634b75febe267ccfb0834
marielfrank <marielbeth@gmail.com>
2017-10-31 14:49:49 -0400

    Add category  controller #index and #show, add 'Add to Cart' on categories#show.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,10 @@
 class CategoriesController < ApplicationController
-
+    def index
+        @categories = Category.all
+    end
+    
+    def show
+        @category = Category.find_by(id: params[:id])
+        @line_item = LineItem.new
+    end
 end
