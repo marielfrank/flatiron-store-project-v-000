b08a75b55412e425007bd549062d4c434033d489
marielfrank <marielbeth@gmail.com>
2017-10-31 11:56:45 -0400

    Create #total, start #add_item for cart.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,20 @@
 class Cart < ActiveRecord::Base
     has_many :line_items
     has_many :items, :through => :line_items
+
+    def total
+        prices = items.collect {|item| item.price}
+        prices.reduce {|sum, price| sum + price}
+    end
+
+    def add_item(item_id)
+        item = Item.find_by(id: item_id)
+        li = LineItem.find_by(cart_id: self.id, item_id: item.id)
+        if li
+            li.quantity += 1
+        else
+            li = LineItem.new(cart_id: self.id, item_id: item.id)
+        end
+        li.save
+    end
 end
