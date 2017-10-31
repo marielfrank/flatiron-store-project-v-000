ce5b681d232eb8cb646bdac83e381a731d38a0eb
marielfrank <marielbeth@gmail.com>
2017-10-31 12:57:50 -0400

    Fix  Item.available_items, fix category association for Item.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ActiveRecord::Base
-    has_many :categories
+    belongs_to :category
     has_many :line_items
     has_many :carts, :through => :line_items
 
     def self.available_items
-        self.detect {|item| !!item.inventory }
+        self.all.select {|item| item.inventory > 0}
     end
 end
